a1515f21a4dbdb30ae3ef6140e2605c732eff7ad
lyn <3228562873@qq.com>
2025-02-17 14:49:17 +0800

    项目初始化，用户模块增删改查

---
+++ src/main/java/com/lyn/npicturebackend/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.lyn.npicturebackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lyn.npicturebackend.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
